0d20617802d22974359bcae9aad0ca83b7654989
viveknhc <viveknhc@gmail.com>
2023-06-14 13:31:47 +0530

    14/06/23

---
+++ assets/js/owl.js
@@ -0,0 +1,19 @@
+$('#banner').owlCarousel({
+    // Configure the options for your carousel
+    items: 1,
+    loop: true,
+    nav: false,
+    dots: true,
+    autoplay: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 1
+        },
+        1024: {
+            items: 1
+        }
+    }
+})
