e3a3ebcbcd559101e94eaaf70b91a0996de69216
viveknhc <viveknhc@gmail.com>
2023-06-15 13:03:05 +0530

    15/06/23

---
+++ assets/js/parallax.js
@@ -0,0 +1,21 @@
+// Hero parallax
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+    ScrollTrigger.create({
+        trigger: panel,
+        start: "top top",
+        scrub: true,
+        pin: true,
+        pinSpacing: false
+    });
+});
+
+
+ScrollTrigger.create({
+    // snap: {
+    //   snapTo: 1 / 4,
+    //   duration: 0.5
+    // }
+});
